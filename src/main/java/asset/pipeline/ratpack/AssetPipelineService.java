7c9e4d1b2a0a8f2953a1f0465e9930e0a1a91a22
Dan Woods <daniel.p.woods@gmail.com>
2015-07-14 14:32:51 -0400

    allow asset source path to be specified for the development-time FileSystemResolver

--- src/main/java/asset/pipeline/ratpack/AssetPipelineService.java
+++ src/main/java/asset/pipeline/ratpack/AssetPipelineService.java
@@ -28,29 +28,41 @@ import java.nio.file.Path;
 import java.util.Properties;
 
 /**
-* This service provides a startup configuration binding to tell the AP Config where to look for files
-* @author David Estes
-*/
+ * This service provides a startup configuration binding to tell the AP Config where to look for files
+ *
+ * @author David Estes
+ */
 public class AssetPipelineService implements Service {
-    public void onStart(StartEvent startEvent) throws Exception {
-        FileSystemBinding fileSystemBinding = startEvent.getRegistry().get(FileSystemBinding.class);
-        AssetPipelineModule.Config config = startEvent.getRegistry().get(AssetPipelineModule.Config.class);
-        
-        if(config != null && config.getAssets() != null) {
-            AssetPipelineConfigHolder.config = config.getAssets();
-        }
-        Path path = fileSystemBinding.getFile();
-        Path manifest = fileSystemBinding.file("assets/manifest.properties");
-        if(manifest != null && Files.exists(manifest)) {
-            // We are in production mode
-            AssetPipelineConfigHolder.config.put("precompiled",true);
-            Properties manifestProps = new Properties();
-            InputStream manIs = Files.newInputStream(manifest);
-            manifestProps.load(manIs);
-            manIs.close();
-            AssetPipelineConfigHolder.manifest = manifestProps;
-        } else {
-            AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver("application",path.toString() + "/../assets"));
-        }
+
+  private final String assetSourcePath;
+
+  public AssetPipelineService() {
+    this("/../assets");
+  }
+
+  public AssetPipelineService(String assetSourcePath) {
+    this.assetSourcePath = assetSourcePath.startsWith("/") ? assetSourcePath : "/" + assetSourcePath;
+  }
+
+  public void onStart(StartEvent startEvent) throws Exception {
+    FileSystemBinding fileSystemBinding = startEvent.getRegistry().get(FileSystemBinding.class);
+    AssetPipelineModule.Config config = startEvent.getRegistry().get(AssetPipelineModule.Config.class);
+
+    if (config != null && config.getAssets() != null) {
+      AssetPipelineConfigHolder.config = config.getAssets();
     }
+    Path path = fileSystemBinding.getFile();
+    Path manifest = fileSystemBinding.file("assets/manifest.properties");
+    if (manifest != null && Files.exists(manifest)) {
+      // We are in production mode
+      AssetPipelineConfigHolder.config.put("precompiled", true);
+      Properties manifestProps = new Properties();
+      InputStream manIs = Files.newInputStream(manifest);
+      manifestProps.load(manIs);
+      manIs.close();
+      AssetPipelineConfigHolder.manifest = manifestProps;
+    } else {
+      AssetPipelineConfigHolder.registerResolver(new FileSystemAssetResolver("application", path.resolve(assetSourcePath).toFile().getCanonicalPath()));
+    }
+  }
 }
